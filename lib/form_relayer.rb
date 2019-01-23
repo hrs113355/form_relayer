4425c128cdf79725a56f33f9989e93f678dfd8c0
hrs <hrs113355@gmail.com>
2019-01-23 17:48:24 +0800

    allow passing excepts tags

--- lib/form_relayer.rb
+++ lib/form_relayer.rb
@@ -2,12 +2,14 @@ require "form_relayer/version"
 
 module FormRelayer
   module ViewHelper
-    def relay_received_post_params
+    def relay_received_post_params(options = {})
       tags = []
 
       raw_params= Rack::Utils.parse_query(request.raw_post)
       raw_params.delete('utf8')
       raw_params.delete('authenticity_token')
+      options[:excepts].each { |except_tag| raw_params.delete(except_tag) } if options[:excepts].present?
+
       raw_params.each do |name, value|
         if value.is_a?(Array)
           value.each { |v| tags << hidden_field_tag(name, v) }
